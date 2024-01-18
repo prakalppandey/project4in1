821593286fec5c250ef0ed5d67ee7db741341fa0
Prakalp Pandey <48084207+prakalppandey@users.noreply.github.com>
2024-01-18 11:31:11 +0000

    project 1st of 4 way two

--- project1/project1.js
+++ project1/project1.js
@@ -1,5 +1,29 @@
 
     
-    function colour11(textbox){
-       document.querySelector('.container').style.backgroundColor = textbox;
-    }
+   //-------------------- way one-------------------------
+
+   //  function colour11(textbox){
+   //     document.querySelector('.container').style.backgroundColor = textbox;
+   //  }
+
+   // ============way two=========================
+   const anchor = document.querySelectorAll('a');
+   const containercolor = document.querySelector('.container');
+
+   anchor.forEach(function(a){
+      a.addEventListener('click',function(event){
+         if(event.target.id === 'Darkgoldenrod'){
+            containercolor.style.backgroundColor = event.target.id
+         }
+         if(event.target.id === 'Crimson'){
+            containercolor.style.backgroundColor = event.target.id
+         }
+         if(event.target.id === 'Bisque'){
+            containercolor.style.backgroundColor = event.target.id
+         }
+         if(event.target.id === 'Khaki'){
+            containercolor.style.backgroundColor = event.target.id
+         }
+      })
+  
+   })
